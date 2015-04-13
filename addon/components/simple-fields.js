2a84ba3161501ff697e59d6be305729aa893276a
Piotrek Bator <piotrek@nibynic.com>
2015-04-13 19:09:08 +0200

    simple-fields component.

---
+++ addon/components/simple-fields.js
@@ -0,0 +1,5 @@
+import SimpleFields from "ember-simple-form/components/simple-form";
+
+export default SimpleFields.extend({
+  tagName: "div"
+});
---
+++ app/components/simple-fields.js
@@ -0,0 +1,4 @@
+import Ember from "ember";
+import SimpleFields from "ember-simple-form/components/simple-fields";
+
+export default SimpleFields;
