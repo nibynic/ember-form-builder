a64e3f6fa8e7ab5c0106ddaf9a2597577742b7d7
Piotrek Bator <piotrek@nibynic.com>
2015-07-21 10:35:36 +0200

    Unnecessary method removed.

--- addon/models/form-builder.js
+++ addon/models/form-builder.js
@@ -22,20 +22,6 @@ export default Ember.Object.extend({
     }
   },
 
-  name: Ember.computed(function(key, value, previousValue) {
-    var newValue = previousValue;
-
-    if (arguments.length > 1) {
-      newValue = value;
-    }
-
-    if (Ember.isEmpty(newValue)) {
-      newValue = this.get("object.constructor");
-    }
-
-    return newValue;
-  }),
-
   model: Ember.computed("object", function() {
     return findModel(this.get("object"));
   }),
