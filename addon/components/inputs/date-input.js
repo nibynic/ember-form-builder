69ddd13ad755ac584f5dc02ffc17b672ef624af7
Paweł Bator <jembezmamy@gmail.com>
2016-01-02 13:53:12 +0100

    simple date input

--- addon/components/inputs/date-input.js
+++ addon/components/inputs/date-input.js
@@ -1,6 +1,32 @@
 import Ember from "ember";
 import InputDefaultsMixin from "ember-simple-form/mixins/input-defaults";
 
+function formatDate(date) {
+  if (date && date.toISOString()) {
+    return date.toISOString().substring(0, 10);
+  } else {
+    return null;
+  }
+}
+
 export default Ember.TextField.extend(InputDefaultsMixin, {
-  type: "date"
+  type: "date",
+  
+  value: Ember.computed("modelValue", {
+    get() {
+      var date = this.get("modelValue");
+      return formatDate(date);
+    },
+    set(key, value) {
+      var timestamp = Date.parse(value);
+      if (isNaN(timestamp)) {
+        this.set("modelValue", null);
+        return null;
+      } else {
+        var date = new Date(timestamp);
+        this.set("modelValue", date);
+        return formatDate(date);
+      }
+    }
+  })
 });
