6b61000b2fefedbb629bb4179d320373f999d9be
Sean <seangores@gmail.com>
2017-01-29 03:58:42 -0700

    simple-label: add field to configure class "labelClass" in config

--- addon/components/simple-label.js
+++ addon/components/simple-label.js
@@ -3,7 +3,17 @@ import Ember from "ember";
 export default Ember.Component.extend({
   translationService: Ember.inject.service("formBuilderTranslations"),
   tagName: "label",
+  configuration: {
+    labelClass: "label" // needs to mirror the config layout in simple-input
+  },
+  classNames: Ember.computed.alias("configuration.labelClass"),
   attributeBindings: ["for"],
+  init() {
+    // TODO: put this config logic into a service
+    let config = Ember.getOwner(this).resolveRegistration("config:environment");
+    let formBuilderConfig = config["formBuilder"];
+    if (!Ember.isEmpty(formBuilderConfig)) this.set("configuration", formBuilderConfig);
+  },
 
   requiredText: Ember.computed(function() {
     var result;
