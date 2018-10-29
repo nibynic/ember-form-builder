00cb956ba59dea814b9f724e67f49448df63fce6
Piotrek Bator <piotrekbator@users.noreply.github.com>
2018-10-29 10:04:12 +0100

    Fixed formatting dates for date input

--- addon/components/inputs/date-input.js
+++ addon/components/inputs/date-input.js
@@ -2,16 +2,18 @@ import { computed } from '@ember/object';
 import TextField from '@ember/component/text-field';
 import InputDefaultsMixin from "ember-form-builder/mixins/input-defaults";
 
-function pad(number) {
-  if (number < 10) {
-    return "0" + number;
+function pad(number, length = 2) {
+  let result = number.toString();
+  length = length - result.length;
+  if (length > 0) {
+    return Array(length + 1).join('0') + result;
   }
-  return number;
+  return result;
 }
 
 function formatDate(date) {
   if (date && date.getFullYear && date.getMonth && date.getDate) {
-    return date.getFullYear() +
+    return pad(date.getFullYear(), 4) +
         "-" + pad(date.getMonth() + 1) +
         "-" + pad(date.getDate());
   } else {
@@ -30,8 +32,7 @@ export default TextField.extend(InputDefaultsMixin, {
     set(key, value) {
       var timestamp = Date.parse(value);
       if (isNaN(timestamp)) {
-        this.set("modelValue", null);
-        return null;
+        return value;
       } else {
         var date = new Date(timestamp);
         this.set("modelValue", date);
