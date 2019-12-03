d901b8d449770e35cfa4c83418ffa5e49e537fd4
Paweł <jembezmamy@users.noreply.github.com>
2019-12-03 13:29:04 +0100

    fix ember 3.4 compatibility

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -3,10 +3,11 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-export default class App extends Application {
-  modulePrefix = config.modulePrefix;
-  podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
-}
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver
+});
 
 loadInitializers(App, config.modulePrefix);
+
+export default App;
