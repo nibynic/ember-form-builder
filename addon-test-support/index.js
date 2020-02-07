f2a4503647627670f1f6ca9fef276be83392233a
Paweł <jembezmamy@users.noreply.github.com>
2020-02-07 12:46:42 +0100

    allow registering custom accessors

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -2,3 +2,6 @@ export { default as fillForm } from './fill-form';
 export { default as readForm } from './read-form';
 export { default as readErrors } from './read-errors';
 export { default as pick } from './pick';
+
+import Registry from './accessors/registry';
+export const registerAccessor = Registry.register;
