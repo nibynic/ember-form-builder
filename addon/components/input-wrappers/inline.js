4fcaef01d15fe835eb4e9d36e4a90f886ae62295
Paweł <jembezmamy@users.noreply.github.com>
2020-04-20 09:50:10 +0200

    glimmerify input wrappers

--- addon/components/input-wrappers/inline.js
+++
@@ -1,8 +0,0 @@
-import classic from 'ember-classic-decorator';
-import { layout as templateLayout } from '@ember-decorators/component';
-import Component from '@ember/component';
-import layout from '../../templates/components/input-wrappers/inline';
-
-@classic
-@templateLayout(layout)
-export default class Inline extends Component {}
