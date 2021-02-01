beb905640042c4742b8584e7a1e4a17f254905fb
Paweł <jembezmamy@users.noreply.github.com>
2021-02-01 10:54:40 +0100

    allow custom key in collection input

--- addon/components/inputs/select-option.js
+++ addon/components/inputs/select-option.js
@@ -1,16 +1,29 @@
 import Component from '@glimmer/component';
 import { computed } from '@ember/object';
-import { isArray } from '@ember/array';
+import { isArray, A } from '@ember/array';
 
 export default class SelectOption extends Component {
 
-  @computed('args.{selectedValue.[],content.content}')
+  @computed('args.{selectedValue.[],content.content,key}')
   get isSelected() {
+    let key = this.args.key;
     if (isArray(this.args.selectedValue)) {
-      return this.args.selectedValue.includes(this.args.content.content);
+      return !!A(this.args.selectedValue).map(
+        (obj) => unwrap(obj, key)
+      ).includes(
+        unwrap(this.args.content.content, key)
+      );
     } else {
-      return this.args.selectedValue === this.args.content.content;
+      return unwrap(this.args.selectedValue, key) === unwrap(this.args.content.content, key);
     }
   }
 
+}
+
+function unwrap(obj, key) {
+  if (key) {
+    return (obj || {})[key];
+  } else {
+    return obj;
+  }
 }
--- tests/integration/components/inputs/collection-input-test.js
+++ tests/integration/components/inputs/collection-input-test.js
@@ -92,94 +92,158 @@ module('Integration | Component | inputs/collection-input', function(hooks) {
   module('collection of objects', function(hooks) {
     hooks.beforeEach(async function() {
       this.collection = [{
-        value: 1, label: 'Cooking', content: {}
+        value: 1, label: 'Cooking', content: { id: '1' }
       }, {
-        value: 2, label: 'Sports', content: {}
+        value: 2, label: 'Sports', content: { id: '2' }
       }, {
-        value: 3, label: 'Politics', content: {}
+        value: 3, label: 'Politics', content: { id: '3' }
       }];
     });
 
-    module('single select', function(hooks) {
-      hooks.beforeEach(async function() {
-        this.set('config', {
-          value:      this.collection[1].content,
-          collection: this.collection
+    module('without key', function(hooks) {
+      module('single select', function(hooks) {
+        hooks.beforeEach(async function() {
+          this.set('config', {
+            value:      this.collection[1].content,
+            collection: this.collection
+          });
+
+          await render(hbs`
+            <Inputs::CollectionInput @config={{config}} />
+          `);
         });
 
-        await render(hbs`
-          <Inputs::CollectionInput @config={{config}} />
-        `);
+        test('it renders', async function(assert) {
+          let options = this.element.querySelectorAll('option');
+
+          assert.dom('select').doesNotHaveAttribute('multiple');
+          assert.dom(options[0]).hasText('Cooking');
+          assert.dom(options[1]).hasText('Sports');
+          assert.dom(options[2]).hasText('Politics');
+          assert.dom(options[0]).hasAttribute('value', '1');
+          assert.dom(options[1]).hasAttribute('value', '2');
+          assert.dom(options[2]).hasAttribute('value', '3');
+        });
+
+        test('it selects given values', async function(assert) {
+          assert.dom('option:checked').exists({ count: 1 });
+          assert.dom('option[value="2"]').matchesSelector(':checked');
+
+          this.set('config.value', this.collection[2].content);
+          await settled();
+
+          assert.dom('option:checked').exists({ count: 1 });
+          assert.dom('option[value="3"]').matchesSelector(':checked');
+        });
+
+        test('it sets the value after being displayed', async function(assert) {
+          this.set('config.value', undefined);
+          await render(hbs`
+            <Inputs::CollectionInput @config={{config}} />
+          `);
+
+          assert.equal(this.config.value, this.collection[0].content);
+        });
       });
 
-      test('it renders', async function(assert) {
-        let options = this.element.querySelectorAll('option');
+      module('multiple select', function(hooks) {
+        hooks.beforeEach(async function() {
+          this.set('config', {
+            value:      [this.collection[1].content, this.collection[2].content],
+            collection: this.collection
+          });
 
-        assert.dom('select').doesNotHaveAttribute('multiple');
-        assert.dom(options[0]).hasText('Cooking');
-        assert.dom(options[1]).hasText('Sports');
-        assert.dom(options[2]).hasText('Politics');
-        assert.dom(options[0]).hasAttribute('value', '1');
-        assert.dom(options[1]).hasAttribute('value', '2');
-        assert.dom(options[2]).hasAttribute('value', '3');
-      });
+          await render(hbs`
+            <Inputs::CollectionInput @config={{config}} />
+          `);
+        });
 
-      test('it selects given values', async function(assert) {
-        assert.dom('option:checked').exists({ count: 1 });
-        assert.dom('option[value="2"]').matchesSelector(':checked');
+        test('it renders', async function(assert) {
+          assert.dom('select').hasAttribute('multiple');
+        });
 
-        this.set('config.value', this.collection[2].content);
-        await settled();
+        test('it selects given values', async function(assert) {
+          assert.dom('option:checked').exists({ count: 2 });
+          assert.dom('option[value="2"]').matchesSelector(':checked');
+          assert.dom('option[value="3"]').matchesSelector(':checked');
 
-        assert.dom('option:checked').exists({ count: 1 });
-        assert.dom('option[value="3"]').matchesSelector(':checked');
-      });
+          this.config.value.pushObject(this.collection[0].content);
+          await settled();
 
-      test('it sets the value after being displayed', async function(assert) {
-        this.set('config.value', undefined);
-        await render(hbs`
-          <Inputs::CollectionInput @config={{config}} />
-        `);
+          assert.dom('option:checked').exists({ count: 3 });
+          assert.dom('option[value="1"]').matchesSelector(':checked');
+        });
 
-        assert.equal(this.config.value, this.collection[0].content);
+        test('it does not set the value after being displayed', async function(assert) {
+          this.set('config.value', []);
+          await render(hbs`
+            <Inputs::CollectionInput @config={{config}} />
+          `);
+
+          assert.deepEqual(this.config.value, []);
+        });
       });
     });
 
-    module('multiple select', function(hooks) {
-      hooks.beforeEach(async function() {
-        this.set('config', {
-          value:      [this.collection[1].content, this.collection[2].content],
-          collection: this.collection
+    module('with key', function(hooks) {
+      module('single select', function(hooks) {
+        hooks.beforeEach(async function() {
+          this.set('config', {
+            value:      { id: '2' },
+            collection: this.collection,
+            key:        'id'
+          });
+
+          await render(hbs`
+            <Inputs::CollectionInput @config={{config}} />
+          `);
         });
 
-        await render(hbs`
-          <Inputs::CollectionInput @config={{config}} />
-        `);
+        test('it selects given values', async function(assert) {
+          assert.dom('option:checked').exists({ count: 1 });
+          assert.dom('option[value="2"]').matchesSelector(':checked');
+
+          this.set('config.value', { id: '3' });
+          await settled();
+
+          assert.dom('option:checked').exists({ count: 1 });
+          assert.dom('option[value="3"]').matchesSelector(':checked');
+        });
+
+        test('it sets the value after being displayed', async function(assert) {
+          this.set('config.value', undefined);
+          await render(hbs`
+            <Inputs::CollectionInput @config={{config}} />
+          `);
+
+          assert.equal(this.config.value, this.collection[0].content);
+        });
       });
 
-      test('it renders', async function(assert) {
-        assert.dom('select').hasAttribute('multiple');
-      });
+      module('multiple select', function(hooks) {
+        hooks.beforeEach(async function() {
+          this.set('config', {
+            value:      [{ id: '2' }, { id: '3' }],
+            collection: this.collection,
+            key:        'id'
+          });
 
-      test('it selects given values', async function(assert) {
-        assert.dom('option:checked').exists({ count: 2 });
-        assert.dom('option[value="2"]').matchesSelector(':checked');
-        assert.dom('option[value="3"]').matchesSelector(':checked');
+          await render(hbs`
+            <Inputs::CollectionInput @config={{config}} />
+          `);
+        });
 
-        this.config.value.pushObject(this.collection[0].content);
-        await settled();
+        test('it selects given values', async function(assert) {
+          assert.dom('option:checked').exists({ count: 2 });
+          assert.dom('option[value="2"]').matchesSelector(':checked');
+          assert.dom('option[value="3"]').matchesSelector(':checked');
 
-        assert.dom('option:checked').exists({ count: 3 });
-        assert.dom('option[value="1"]').matchesSelector(':checked');
-      });
+          this.config.value.pushObject({ id: '1' });
+          await settled();
 
-      test('it does not set the value after being displayed', async function(assert) {
-        this.set('config.value', []);
-        await render(hbs`
-          <Inputs::CollectionInput @config={{config}} />
-        `);
-
-        assert.deepEqual(this.config.value, []);
+          assert.dom('option:checked').exists({ count: 3 });
+          assert.dom('option[value="1"]').matchesSelector(':checked');
+        });
       });
     });
   });
