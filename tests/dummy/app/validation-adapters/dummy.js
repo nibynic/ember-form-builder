01ff14f37b8fd5fa35d5b8ca7a05a8341269e570
Paweł <720354+jembezmamy@users.noreply.github.com>
2022-08-01 14:08:57 +0200

    fix 3.16 compatibility

--- tests/dummy/app/validation-adapters/dummy.js
+++ tests/dummy/app/validation-adapters/dummy.js
@@ -1,13 +1,17 @@
 import EmberObject from '@ember/object';
 import classic from 'ember-classic-decorator';
+import { dependentKeyCompat } from '@ember/object/compat';
 
 @classic
 export default class DummyValidationAdapter extends EmberObject {
   validate() {
-    return this.object.validate();
+    if (this.object?.validate) {
+      return this.object.validate();
+    }
   }
 
+  @dependentKeyCompat
   get attributes() {
-    return this.object.validationsDummy;
+    return this.object?.validationsDummy;
   }
 }
