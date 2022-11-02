f88608051f2598a6e04d9e6966dbbc1be58273c8
Paweł <720354+jembezmamy@users.noreply.github.com>
2022-11-02 14:23:02 +0100

    embroider-optimized support
    removed circular dependency between form-builder and form-builder/fields components

--- addon/src/components/form-builder.js
+++ addon/src/components/form-builder.js
@@ -2,10 +2,9 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { getOwner } from '@ember/application';
 import optional from '../utils/optional-action';
-import { tracked } from '@glimmer/tracking';
-import { dependentKeyCompat } from '@ember/object/compat';
+import { Settings } from './form-builder/fields';
 
-export default class FormBuilder extends Component {
+export default class FormBuilderComponent extends Component {
   novalidate = false;
 
   @optional action() {}
@@ -51,57 +50,5 @@ export default class FormBuilder extends Component {
     this.formBuilder = getOwner(this)
       .factoryFor('model:form-builder')
       .create({ settings: this.settings });
-  }
-}
-
-const NOT_SET = Math.random();
-
-class Settings {
-  @tracked source;
-
-  constructor(source) {
-    this.source = source;
-  }
-
-  @dependentKeyCompat
-  get object() {
-    return this.source.for;
-  }
-
-  @dependentKeyCompat
-  get modelName() {
-    return this.source.name;
-  }
-
-  @dependentKeyCompat
-  get translationKey() {
-    return this.source.translationKey;
-  }
-
-  @dependentKeyCompat
-  get index() {
-    return this.source.index;
-  }
-
-  @tracked _isLoading = NOT_SET;
-
-  @dependentKeyCompat
-  get isLoading() {
-    return (
-      (this._isLoading !== NOT_SET && this._isLoading) || this.source.isLoading
-    );
-  }
-  set isLoading(v) {
-    this._isLoading = v;
-  }
-
-  @tracked _status = NOT_SET;
-
-  @dependentKeyCompat
-  get status() {
-    return (this._status !== NOT_SET && this._status) || this.source.status;
-  }
-  set status(v) {
-    this._status = v;
   }
 }
--- addon/src/components/form-builder/fields.js
+++ addon/src/components/form-builder/fields.js
@@ -1,7 +1,10 @@
-import FormBuilder from '../form-builder';
+import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { dependentKeyCompat } from '@ember/object/compat';
+import { getOwner } from '@ember/application';
 
-export default class Fields extends FormBuilder {
+export default class FieldsComponent extends Component {
   @action
   initializeBuilder() {
     this.args.on.addChild(this.formBuilder);
@@ -10,5 +13,65 @@ export default class Fields extends FormBuilder {
   @action
   destroyBuilder() {
     this.args.on.removeChild(this.formBuilder);
+  }
+
+  constructor() {
+    super(...arguments);
+    this.settings = new Settings(this.args);
+    this.formBuilder = getOwner(this)
+      .factoryFor('model:form-builder')
+      .create({ settings: this.settings });
+  }
+}
+
+const NOT_SET = Math.random();
+
+export class Settings {
+  @tracked source;
+
+  constructor(source) {
+    this.source = source;
+  }
+
+  @dependentKeyCompat
+  get object() {
+    return this.source.for;
+  }
+
+  @dependentKeyCompat
+  get modelName() {
+    return this.source.name;
+  }
+
+  @dependentKeyCompat
+  get translationKey() {
+    return this.source.translationKey;
+  }
+
+  @dependentKeyCompat
+  get index() {
+    return this.source.index;
+  }
+
+  @tracked _isLoading = NOT_SET;
+
+  @dependentKeyCompat
+  get isLoading() {
+    return (
+      (this._isLoading !== NOT_SET && this._isLoading) || this.source.isLoading
+    );
+  }
+  set isLoading(v) {
+    this._isLoading = v;
+  }
+
+  @tracked _status = NOT_SET;
+
+  @dependentKeyCompat
+  get status() {
+    return (this._status !== NOT_SET && this._status) || this.source.status;
+  }
+  set status(v) {
+    this._status = v;
   }
 }
