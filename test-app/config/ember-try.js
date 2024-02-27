0ce6a426686433e66594aa4faf92bd646165bf4a
Felix Fichte <dev@spruce.de>
2024-02-27 13:05:46 +0100

    Adding minimum versions

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -8,6 +8,22 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
